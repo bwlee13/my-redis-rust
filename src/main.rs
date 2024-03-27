1741dcd946543b69c17b489a352ebc58a9d0efc6
bwlee13 <brandonlee2013@gmail.com>
2024-03-27 15:07:30 -0400

    return pong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    ptr::write_bytes,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,12 +11,26 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
+                handle_conn(stream);
                 println!("accepted new connection");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+pub fn handle_conn(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    loop {
+        let bytes_read = stream
+            .read(&mut buffer)
+            .expect("Failed ot read from stream");
+        if bytes_read == 0 {
+            return;
+        }
+        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
