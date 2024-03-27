cedefa8755286e7ac0be9734f19dcf03e3bada29
bwlee13 <brandonlee2013@gmail.com>
2024-03-27 15:47:40 -0400

    multi threaded

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,50 @@
+use anyhow::bail;
 use std::{
     io::{Read, Write},
+    net::SocketAddr,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    select,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let stream: std::io::Result<(TcpStream, SocketAddr)> = listener.accept().await;
         match stream {
-            Ok(stream) => {
-                handle_conn(stream);
-                println!("accepted new connection");
+            Ok((stream, addr)) => {
+                println!("accepted new connection on {:?}", addr);
+                handle_conn(stream).await?;
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => bail!("Failed to accept connection: {}", e),
         }
     }
 }
 
-pub fn handle_conn(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    loop {
-        let bytes_read = stream
-            .read(&mut buffer)
-            .expect("Failed ot read from stream");
-        if bytes_read == 0 {
-            return;
+async fn handle_conn(mut stream: TcpStream) -> anyhow::Result<()> {
+    tokio::spawn(async move {
+        let mut buffer = [0; 1024];
+        loop {
+            select! {
+                Ok(bytes_read) = stream.read(&mut buffer) => {
+                        if bytes_read == 0 {
+                            break;
+                        }
+                        if let Err(e) = stream.write_all(b"+PONG\r\n").await {
+                            eprintln!("Failed to write TCP Stream: {}", e);
+                            break;
+                        }
+                    }
+                else => {
+                    break
+                }
+            }
         }
-
-        stream.write_all(b"+PONG\r\n").unwrap();
-    }
+    });
+    Ok(())
 }
