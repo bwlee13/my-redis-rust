1459ffc472e4329ca78d9016bae0d8b8bce0b303
bwlee13 <brandonlee2013@gmail.com>
2024-04-01 08:01:51 -0400

    expiry addition

--- src/server.rs
+++ src/server.rs
@@ -8,6 +8,7 @@ use std::time::Instant;
 pub struct RedisItem {
     value: String,
     created_at: Instant,
+    expiration: Option<i64>,
 }
 
 #[derive(Debug, Clone)]
@@ -27,10 +28,27 @@ impl Server {
         let value = unpack_bulk_str(args.get(1).unwrap().clone()).unwrap();
         let mut cache = self.cache.lock().unwrap();
 
+        let expiration = match args.get(2) {
+            None => None,
+            Some(Value::BulkString(sub_cmd)) => {
+                if sub_cmd != "px" {
+                    panic!("Invalid command for expiration time")
+                }
+                match args.get(3) {
+                    None => None,
+                    Some(Value::BulkString(time)) => Some(time.parse::<i64>().unwrap()),
+                    _ => panic!("Invalid expiration time"),
+                }
+            }
+            _ => panic!("Invalid expiration time"),
+        };
+
         let redis_item = RedisItem {
             value,
             created_at: Instant::now(),
+            expiration,
         };
+
         cache.insert(key, redis_item);
 
         Value::SimpleString("OK".to_string())
@@ -41,7 +59,18 @@ impl Server {
         let cache = self.cache.lock().unwrap();
 
         match cache.get(&key) {
-            Some(value) => Value::SimpleString(value.value.clone()),
+            Some(value) => {
+                if let Some(expiration) = value.expiration {
+                    let now = Instant::now();
+                    if now.duration_since(value.created_at).as_millis() > expiration as u128 {
+                        Value::NullBulkString
+                    } else {
+                        Value::SimpleString(value.value.clone())
+                    }
+                } else {
+                    Value::SimpleString(value.value.clone())
+                }
+            }
             None => Value::NullBulkString,
         }
     }
